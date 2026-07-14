3289035c1585ff7c0dadc4a5816d2b804d9aa30c
EthanMott <ethanrmott@gmail.com>
2026-07-14 18:33:52 +0100

    Database replaced

--- Run.bat
+++ Run.bat
@@ -1,12 +1,13 @@
 @echo off
 
+cd /d "%~dp0"
 javac -cp ".;sqlite-jdbc-3.53.2.0.jar" src\Main.java src\GUI.java
-
 if errorlevel 1 (
-    pause
-    exit /b
+	echo Compilation failed!
+	pause
+	exit /b
 )
 
-start "" javaw -cp ".;sqlite-jdbc-3.53.2.0.jar;src" Main
+start "" javaw -cp "src;sqlite-jdbc-3.53.2.0.jar" Main
 
 exit
